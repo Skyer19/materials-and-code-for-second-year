718f64be2df6bcee08f7c9816459cea3476bcf60
mengyu666 <rmy200929@163.com>
2021-05-11 11:15:02 +0800

    upload

---
+++ Operating Systems, Communications & Concurrency/practical8/CS240 Practical 8c/src/Semaphore.java
@@ -0,0 +1,31 @@
+public class Semaphore {
+	private int value;
+
+	public Semaphore(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public synchronized void acquire() {
+		while (value == 0) {
+			try {
+// Calling thread waits until semaphore is free 
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		value = value - 1;
+	}
+
+	public synchronized void release() {
+		value = value + 1;
+		notify();
+	}
+}
